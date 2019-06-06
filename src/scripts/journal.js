3f39a63421b44603e9ed30545ce4d01592ed29a4
berkeley platte <berkeleyplatte989@gmail.com>
2019-06-06 13:48:19 -0500

    got the functions to work

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,5 +1,43 @@
-const objectsJournalEntry = {
-    
+function cl(data) {
+  console.log(data);
 }
+cl("working");
 
-const journalEntries = [].push(journalEntry);
+let journalEntries = [];
+
+let sixSix = {
+  date: "6/6",
+  concepts: "api and callback functions",
+  content: "today did more api work and learned about callback functions",
+  mood: "fine"
+};
+let sixFive = {
+  date: "6/5",
+  concepts: "api intro",
+  content: "yesterday we were introduced to api",
+  mood: "fine"
+};
+let sixFour = {
+  date: "6/4",
+  concepts: "more js",
+  content: "two days ago we did more js",
+  mood: "fine"
+};
+journalEntries.push(sixSix);
+cl(journalEntries);
+
+const makeJournalEntryComponents = journalEntries => {
+  for (let i = 0; i < journalEntries.length; i++) {
+    return `<h1>${journalEntries[i].concepts}</h1>
+            <p>${journalEntries[i].content}</p>
+            <p>${journalEntries[i].date}</p>`;
+  }
+};
+
+const renderJournalEntries = makeJournalEntryComponents => {
+  document.querySelector(".entryLog").innerHTML = makeJournalEntryComponents(
+    journalEntries
+  );
+};
+
+renderJournalEntries(makeJournalEntryComponents);
